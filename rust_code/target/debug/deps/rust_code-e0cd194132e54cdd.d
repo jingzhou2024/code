0a486ff947bcd37690711e0f584c7becf4a1fee5
jingzhou2024 <yuqingyuzhuo@proton.me>
2024-04-08 01:12:43 +0800

    删除目录

--- rust_code/target/debug/deps/rust_code-e0cd194132e54cdd.d
+++
@@ -1,5 +0,0 @@
-C:\my_code\code\rust_code\target\debug\deps\rust_code-e0cd194132e54cdd.exe: src\main.rs
-
-C:\my_code\code\rust_code\target\debug\deps\rust_code-e0cd194132e54cdd.d: src\main.rs
-
-src\main.rs:
--- rust_code/target/debug/rust_code.d
+++
@@ -1,1 +0,0 @@
-C:\my_code\code\rust_code\target\debug\rust_code.exe: C:\my_code\code\rust_code\src\main.rs
