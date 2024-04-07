00fb3452087bf5a39445681339bf815d52847b57
jingzhou2024 <yuqingyuzhuo@proton.me>
2024-04-08 00:39:56 +0800

    第一次提交rust代码

---
+++ rust_code/target/debug/deps/rust_code-e0cd194132e54cdd.d
@@ -0,0 +1,5 @@
+C:\my_code\code\rust_code\target\debug\deps\rust_code-e0cd194132e54cdd.exe: src\main.rs
+
+C:\my_code\code\rust_code\target\debug\deps\rust_code-e0cd194132e54cdd.d: src\main.rs
+
+src\main.rs:
---
+++ rust_code/target/debug/rust_code.d
@@ -0,0 +1,1 @@
+C:\my_code\code\rust_code\target\debug\rust_code.exe: C:\my_code\code\rust_code\src\main.rs
