00ead656b5e6a61ca66fe88b8ea0a80c6adab529
jingzhou2024 <yuqingyuzhuo@proton.me>
2024-04-08 18:53:06 +0800

    学习fmt打印

--- rust_code/src/main.rs
+++ rust_code/src/main.rs
@@ -1,18 +1,61 @@
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-        }
-        m = m % n;
-    }
-    n
+mod display_use;
+mod list_display_use;
+mod format_use;
+
+use log::debug;
+
+fn fmt_use() {
+    // std::fmt 定义的宏处理打印
+    // 实现至少一个可打印的 traits
+    println!("Let's print some Lines:");
+    println!();
+    println!("Hello, world");
+    println!("{}, {}", "hello", "world");
+    println!("Hello, world!");
+    println!("{number:>0width$}", number = 1, width = 6);
+    eprintln!("{subject} {verb} {object}",
+              object = "the lazy dog",
+              subject = "the quick brown fox",
+              verb = "jumps over");
 }
 
-#[test]
-fn test_gcd() {
-    assert_eq!(gcd(14, 15), 1);
-    assert_eq!(gcd(2 * 3 *5 * 11 *17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+fn format_use() {
+    //format!使用
+}
+
+#[derive(Debug)]
+struct Structure(i32);
+
+#[derive(Debug)]
+struct Deep(Structure);
+
+// debug 通过推导实现
+fn debug_use() {
+    // 使用 `derive` 的一个问题是不能控制输出的形式。
+    println!("{:?}", Structure(12));
+    println!("{:?}", Deep(Structure(7)));
+    println!("{1:?} {0:?} is the {actor:?} name.",
+             "Slater",
+             "Christian",
+             actor = "actor's")
+}
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8,
+}
+
+fn beautiful_use() {
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // 美化打印
+    println!("{:#?}", peter);
+}
+fn main() {
+    // println!("{:#?}", display_use::Stru(3));
+    // list_display_use::list_display_use();
+    format_use::format_use();
 }
